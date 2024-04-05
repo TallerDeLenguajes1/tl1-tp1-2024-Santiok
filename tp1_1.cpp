cecf0c961dc2ba1019e1270cde3ab7b6f2f63c83
“Santiago” <santimedinaok.10@gmail.com>
2024-04-05 11:05:39 -0300

    segundo commit

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
 int main() {
+
     printf("Hola Mundo");
+
+    int var = 10, *p_var;
+    p_var = &var;
+
+    printf("\nEl contenido del puntero: %d \n La direccion de memoria almacenada en el puntero: %d \n La direccion de memoria de la variable: %d \n La direccion de memoria del puntero: %d \n El tamaño de memoria utilizado por la variable: %d", *p_var, p_var, &var, &p_var, sizeof(var));
 }
